cfa9b261acc93147afcc3e5566f35df0f039af8a
bctix <51544115+bctix@users.noreply.github.com>
2025-02-04 23:40:44 -0500

    fix indent again

--- src/commands/music/play/globalplay.js
+++ src/commands/music/play/globalplay.js
@@ -9,35 +9,35 @@ async function playSong(command, source) {
         return;
     }
 
-        try {
-            const player = await getPlayer(commandToLavaData(command));
+    try {
+        const player = await getPlayer(commandToLavaData(command));
 
-            if(!player) {
-                command.data.reply("I couldn't get what vc you're in!");
-            }
-
-            var res = await player.search({query: query, source: source}, command.data.member.user);
-            if(!res || !res.tracks?.length)  {
-                command.data.reply("Couldn't find any songs!");
-                return;
-            }
-
-            if (!player.connected) await player.connect();
-
-            await player.queue.add(
-                res.loadType === "playlist" ? res.tracks : res.tracks[0]
-            )
-
-            // Only reply if its a interaction to prevent the error message
-            if(!command.isMessage)
-                command.data.reply({content: player.playing ? "Added your song to the queue!" : "Now playing your song!", flags: MessageFlags.Ephemeral});
-
-            if (!player.playing) 
-                await player.play();
-
-        } catch(e) {
-            console.log(e.message);
+        if(!player) {
+            command.data.reply("I couldn't get what vc you're in!");
         }
+
+        var res = await player.search({query: query, source: source}, command.data.member.user);
+        if(!res || !res.tracks?.length)  {
+            command.data.reply("Couldn't find any songs!");
+            return;
+        }
+
+        if (!player.connected) await player.connect();
+
+        await player.queue.add(
+            res.loadType === "playlist" ? res.tracks : res.tracks[0]
+        )
+
+        // Only reply if its a interaction to prevent the error message
+        if(!command.isMessage)
+            command.data.reply({content: player.playing ? "Added your song to the queue!" : "Now playing your song!", flags: MessageFlags.Ephemeral});
+
+        if (!player.playing) 
+            await player.play();
+
+    } catch(e) {
+        console.log(e.message);
+    }
 }
 
 module.exports = {
