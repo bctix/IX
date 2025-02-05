ff59dc5a5131cbc5ac588b015596bb986f16fa3e
bctix <bctix2006@gmail.com>
2025-02-05 10:40:28 -0500

    Update help command

--- src/commands/info/help.js
+++ src/commands/info/help.js
@@ -11,12 +11,14 @@ module.exports = {
         .setDescription(`All commands can be used as a slash (/) command, or a prefix (${process.env.PREFIX})`)
         .addFields(
             {name: "Play Commands:\n(All these commands play a song)", value: `\`All these commands can use a link OR can be searched via song title\`\n
-                \`play\` - Searches Youtube Music
-                \`spotify\` - Searches Spotify
-                \`youtube\` - Searches normal Youtube
-                \`soundcloud\` - Searches SoundCloud`},
+                \`play\` | \`p\` - Searches Youtube Music
+                \`spotify\` | \`sp\` - Searches Spotify
+                \`youtube\` | \`yt\` - Searches normal Youtube
+                \`soundcloud\` | \`sc\` - Searches SoundCloud`},
             { name: "Music Commands:\n(All the commands to control the music)", value: `
-                \`stop\` - Stops song, clears queue, and leaves VC`}
+                \`stop\` | \`st\` - Stops song, clears queue, and leaves VC
+                \`loop\` | \`l\` (\`single\`|\`queue\`|\`none\`) - Sets the loop mode
+                `}
         )
         const msg = await command.data.reply({embeds: [embed]});
     }
