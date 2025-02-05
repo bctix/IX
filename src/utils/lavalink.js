c576176772e2829389ff1de5446c189fdf82ca94
bctix <51544115+bctix@users.noreply.github.com>
2025-02-04 19:24:55 -0500

    create lavalink manager

--- src/utils/lavalink.js
+++ src/utils/lavalink.js
@@ -6,8 +6,25 @@ const { LavalinkManager } = require("lavalink-client");
 
 async function initLavalink(client) {
     client.lavalink = new LavalinkManager({
-        
-    })
+        nodes: [
+            { 
+                authorization: process.env.LAVALINK_PASSWORD,
+                host: process.env.LAVALINK_HOST,
+                port: parseInt(process.env.LAVALINK_PORT)
+            }
+        ],
+        sendToShard: (guildId, payload) =>
+            client.guilds.cache.get(guildId)?.shard?.send(payload),
+        client: {
+            id: client.user.id,
+            username: client.user.username,
+        },
+        playerOptions: {
+            onEmptyQueue: {
+                destroyAfterMs: 30_000, 
+            }
+        }
+    });
 }
 
 module.exports = {
