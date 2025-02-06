4ef32645e4998c5323e7c4b8a7a5c3fd404bcbad
bctix <bctix2006@gmail.com>
2025-02-06 09:15:01 -0500

    fix echo

--- src/commands/misc/echo.js
+++ src/commands/misc/echo.js
@@ -11,7 +11,7 @@ module.exports = {
     category: "Misc",
 
     execute: async function(command) {
-        const text = command.isMessage ? command.args.join(" ") : command.data.options.getString("echo", true);
-        await command.data.reply(text);
+        const text = command.isMessage ? command.args.join(" ") : command.data.options.getString("text", true);
+        await command.data.reply(text+`\n-# echoing ${command.data.member.user.username}`);
     }
 }
