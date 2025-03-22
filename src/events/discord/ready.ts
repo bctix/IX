d1df783386bdc765f665476772684521f04ff977
bctix <51544115+bctix@users.noreply.github.com>
2025-03-22 19:56:43 -0400

    Initial commit

---
+++ src/events/discord/ready.ts
@@ -0,0 +1,25 @@
+import { Events } from "discord.js";
+import { CustomClient } from "../../types/bot_classes";
+import { deployApplicationCommands } from "../../utils/registry";
+
+export default {
+	name: Events.ClientReady,
+	async execute(Client: CustomClient) {
+		console.log("Bot started!");
+		// console.clear();
+
+		console.log("Deploying slash commands...");
+
+		try {
+			await deployApplicationCommands(Client);
+		}
+		catch (e) {
+			console.error(e);
+		}
+
+		console.log("Success!");
+		// console.clear();
+
+		console.log("Bot is ready!");
+	},
+};
