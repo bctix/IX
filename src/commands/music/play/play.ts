f857e4e62220f81f9eb28d510603b35cfb667480
bctix <51544115+bctix@users.noreply.github.com>
2025-03-23 00:09:48 -0400

    add query option to play

--- src/commands/music/play/play.ts
+++ src/commands/music/play/play.ts
@@ -1,11 +1,20 @@
-import { ChatInputCommandInteraction, Message } from 'discord.js';
+import { ApplicationCommandOptionType, ChatInputCommandInteraction, Message } from 'discord.js';
 import { ChatCommand, ChatCommandOptions, ChatCommandExecute } from '../../../types/bot_classes';
 import globalplay from './globalplay';
 
 const textcommand: ChatCommand = new ChatCommand(
     {
         name: "play",
-        description: "Ping pong!",
+        description: "Play some tunes!",
+        options: [
+            {
+                name: "query",
+                description: "Song title or link.",
+                type: ApplicationCommandOptionType.String,
+                default: "",
+                required: true
+            }
+        ],
         argParser(str: string, message: Message) {
             return [str];
         },
