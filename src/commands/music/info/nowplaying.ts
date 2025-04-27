827c8f8e06ff68c34205999e0516b07dbed5b8f2
bctix <51544115+bctix@users.noreply.github.com>
2025-04-26 23:20:30 -0400

    use components v2 for `nowplaying`

--- src/commands/music/info/nowplaying.ts
+++ src/commands/music/info/nowplaying.ts
@@ -1,7 +1,7 @@
-import { Colors, EmbedBuilder, GuildMember, hyperlink, User } from "discord.js";
+import { ButtonBuilder, ButtonStyle, Colors, ContainerBuilder, GuildMember, hyperlink, MessageFlags, SectionBuilder, SeparatorSpacingSize, TextDisplayBuilder, ThumbnailBuilder, User, userMention } from "discord.js";
 import { ChatCommand, ChatCommandOptions, ChatCommandExecute } from "../../../types/bot_classes";
 import { commandToLavaData, getLavalinkPlayer } from "../../../utils/lavalink";
-import { generateProgressBar, getEmojiFromName, getVibrantColorToDiscord, msToTime } from "../../../utils/utils";
+import { generateProgressBar, getVibrantColorToDiscord, msToTime } from "../../../utils/utils";
 
 const textcommand: ChatCommand = new ChatCommand(
     {
@@ -23,29 +23,118 @@ const textcommand: ChatCommand = new ChatCommand(
     
                 if (!track) {command.data.reply("You are not playing anything!"); return;};
 
-                const embed = new EmbedBuilder();
-                embed.setTitle("Now playing:");
-                embed.setDescription(`${hyperlink(track.info.title, track.info.uri)}`);
-
-                embed.addFields(
-                    { name: "Artist", value: track.info.author, inline: true },
-                    { name: "Source", value: `${getEmojiFromName(command.client, track.info.sourceName)} ${track.info.sourceName}`, inline: true },
-                    { name: "\t", value: "\t", inline: false },
-                    { name: "Requestor", value: `<@${(track.requester as User).id}>`, inline: true },
-                    { name: "Progress", value: `\`${msToTime(player.position)}\` - ${generateProgressBar(player.position / track.info.duration)} - \`${msToTime(track.info.duration)}\``, inline: false },
+                const container = new ContainerBuilder();
+                const topSection = new SectionBuilder();
+                const topTitle = new TextDisplayBuilder().setContent(
+                    [
+                        "### Current song:",
+                        `### ${hyperlink(`${track.info.title}`, track.info.uri)}`,
+                        `${hyperlink(`- ${track.info.author}`, track.info.uri)}`,
+                    ].join("\n")
                 );
 
-                if (track.info.artworkUrl) {
-                    embed.setThumbnail(track.info.artworkUrl);
-    
-                    try {
-                        embed.setColor(await getVibrantColorToDiscord(track.info.artworkUrl));
-                    } catch {
-                        embed.setColor(Colors.Green);
-                    }
-                }
+                topSection.addTextDisplayComponents(topTitle);
 
-                await command.data.reply({embeds: [embed]});
+                if (track.info.artworkUrl) {
+                    const col = await getVibrantColorToDiscord(track.info.artworkUrl);
+                    if (col)
+                        container.setAccentColor(col);
+
+                    const thumbnail = new ThumbnailBuilder().setURL(track.info.artworkUrl);
+                    topSection.setThumbnailAccessory(thumbnail);
+                } else {
+                    container.setAccentColor(Colors.Green);
+                }
+                container.addSectionComponents(topSection);
+                container.addSeparatorComponents(separator => separator.setSpacing(SeparatorSpacingSize.Large));
+
+                const middleText = new TextDisplayBuilder().setContent(
+                    [
+                        `**Duration**:\n${track.info.isStream ? "Live" : msToTime(track.info.duration)}`,
+                        `**Source**:\n${track.info.sourceName}\n`,
+                        `\`${msToTime(player.position)}\` - ${generateProgressBar(player.position / track.info.duration)} - \`${msToTime(track.info.duration)}\`\n`,
+                        `-# Requested by: ${userMention((track.requester as User).id)}`
+                    ].join("\n")
+                );
+
+                container.addTextDisplayComponents(middleText);
+                container.addSeparatorComponents(separator => separator.setSpacing(SeparatorSpacingSize.Large));
+
+                const backButton = new ButtonBuilder()
+                    .setStyle(ButtonStyle.Primary)
+                    .setEmoji("⏮️")
+                    .setCustomId("back");
+
+                const pauseButton = new ButtonBuilder()
+                    .setStyle(ButtonStyle.Secondary)
+                    .setEmoji(player.paused ? "▶️" : "⏸️")
+                    .setCustomId("pause");
+
+                const forwardButton = new ButtonBuilder()
+                    .setStyle(ButtonStyle.Primary)
+                    .setEmoji("⏭️")
+                    .setCustomId("forward");
+
+                const toggleLoop = new ButtonBuilder()
+                    .setStyle(ButtonStyle.Secondary)
+                    .setLabel("Toggle loop")
+                    .setCustomId("toggleLoop");
+
+                container.addActionRowComponents(row => row.addComponents(backButton, pauseButton, forwardButton, toggleLoop));
+
+                const message = await command.data.reply({ 
+                    components: [container],
+                    flags: MessageFlags.IsComponentsV2,
+                });
+
+                const collector = message.createMessageComponentCollector({ filter: i => i.user.id === (track.requester as User).id, time: 240_000 });
+
+                collector.on("collect", async (i) => {
+                    if (i.user.id !== (track.requester as User).id) {
+                        return;
+                    }
+
+                    switch (i.customId) {
+                        case "back": {
+                            // TODO: Implement back to go to tracks in the previous queue.
+                            await player.seek(0);
+                            break;
+                        }
+                        case "pause": {
+                            if (player.paused) {
+                                await player.resume();
+                                pauseButton.setEmoji("⏸️");
+                            } else {
+                                await player.pause();
+                                pauseButton.setEmoji("▶️");
+                            }
+                            break;
+                        }
+                        case "forward": {
+                            await player.skip();
+                            break;
+                        }
+                        case "toggleLoop": {
+                            switch (player.repeatMode) {
+                                case "off":
+                                    await player.setRepeatMode("track");
+                                    toggleLoop.setLabel("Looping track").setStyle(ButtonStyle.Success);
+                                    break;
+                                case "track":
+                                    await player.setRepeatMode("queue");
+                                    toggleLoop.setLabel("Looping queue").setStyle(ButtonStyle.Success);
+                                    break;
+                                case "queue":
+                                    await player.setRepeatMode("off");
+                                    toggleLoop.setLabel("Looping off").setStyle(ButtonStyle.Secondary);
+                                    break;
+                            }
+                            break;
+                        }
+                    }
+
+                    await i.update({ components: [container] });
+                });
             } catch (e) {
                 command.data.reply("Something went wrong!");
                 console.error(e);
