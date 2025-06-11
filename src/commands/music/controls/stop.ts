62576dfa00a58695bbb9c00aafa621c6f0131765
bctix <bctix2006@gmail.com>
2025-06-11 13:25:20 -0400

    add util playercheck and stop command

---
+++ src/commands/music/controls/stop.ts
@@ -0,0 +1,28 @@
+import { ApplicationCommandOptionType, ChatInputCommandInteraction, MessageFlags } from "discord.js";
+import { ChatCommand, ChatCommandExecute, ChatCommandOptions } from "../../../types/bot_types";
+import { checkPlayer, commandToLavaData, getLavalinkPlayer, playSong } from "../../../utils/lavalink";
+
+const textcommand: ChatCommand = new ChatCommand(
+    {
+        name: "stop",
+        description: "Stop some tunes!",
+        aliases: ["st"],
+        usage: "Stops the current song, clears the queue, and leaves VC.",
+        category: "music (play)",
+        argParser(str: string) {
+            return [str];
+        },
+        async execute(command: ChatCommandExecute) {
+            const player = getLavalinkPlayer(commandToLavaData(command));
+
+            if (!checkPlayer(command, player) || !player) return;
+
+            await player.destroy("stopRequest");
+
+            // Only reply if its a interaction to prevent the error message
+            if (!command.isMessage) {await (command.data as ChatInputCommandInteraction).reply({ content: "Stopped playing!", flags: MessageFlags.Ephemeral });}
+        },
+    } as ChatCommandOptions,
+);
+
+export default textcommand;
