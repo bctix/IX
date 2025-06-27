5e4247eb1314fed3e5f30059c501d1004dc18133
bctix <51544115+bctix@users.noreply.github.com>
2025-06-27 13:47:31 -0400

    add awesome buttons to `queue`

--- src/commands/music/queue/queue.ts
+++ src/commands/music/queue/queue.ts
@@ -1,7 +1,36 @@
-import { ApplicationCommandOptionType, ContainerBuilder, hyperlink, MessageFlags, SeparatorBuilder, SeparatorSpacingSize, TextDisplayBuilder } from "discord.js";
+import { ApplicationCommandOptionType, ButtonBuilder, ButtonStyle, ContainerBuilder, GuildMember, hyperlink, MessageFlags, SeparatorBuilder, SeparatorSpacingSize, TextDisplayBuilder } from "discord.js";
 import { ChatCommand, ChatCommandExecute, ChatCommandOptions } from "../../../types/bot_types";
 import { checkPlayer, commandToLavaData, getLavalinkPlayer } from "../../../utils/lavalink";
 import { msToTime } from "../../../utils/utils";
+import { Player } from "lavalink-client/dist/types";
+
+interface QueueData {
+    queueString: string,
+    queueLength: number,
+}
+
+function generateQueueData(player: Player, page: number): QueueData {
+    const slicedQueue = player.queue.tracks.slice((page - 1) * 10, page * 10);
+
+    let queueStr = "";
+    let queueLength = 0;
+
+    slicedQueue.forEach(track => {
+        queueStr += `${player.queue.tracks.indexOf(track) + 1}. ${track.info.uri ? hyperlink(track.info.title, track.info.uri) : track.info.title} - ${track.info.author ?? "Unknown"}\n`;
+    });
+
+    player.queue.tracks.forEach(track => {
+        if (track.info.duration) queueLength += track.info.duration;
+    });
+
+    if (queueStr === "") queueStr = "Empty";
+
+    return {
+        queueLength: queueLength,
+        queueString: queueStr,
+    };
+}
+
 
 const textcommand: ChatCommand = new ChatCommand(
     {
@@ -27,33 +56,94 @@ const textcommand: ChatCommand = new ChatCommand(
             const player = getLavalinkPlayer(commandToLavaData(command));
             if (!checkPlayer(command, player) || !player) return;
 
+
             const desiredPage = command.args[0];
 
-            const slicedQueue = player.queue.tracks.slice((desiredPage - 1) * 10, desiredPage * 10);
+            let curPage = desiredPage;
 
-            let queueStr = "";
-            let queueLength = 0;
+            const queueData = generateQueueData(player, desiredPage);
 
-            slicedQueue.forEach(track => {
-                queueStr += `${player.queue.tracks.indexOf(track) + 1}. ${track.info.uri ? hyperlink(track.info.title, track.info.uri) : track.info.title} - ${track.info.author ?? "Unknown"}\n`;
-
-                if (track.info.duration) queueLength += track.info.duration;
-            });
-
-            if (queueStr === "") queueStr = "Empty";
-
-            const container = new ContainerBuilder();
+            let container = new ContainerBuilder();
 
             container.addTextDisplayComponents(new TextDisplayBuilder({ content: "## Queue:" }));
             container.addSeparatorComponents(new SeparatorBuilder({ spacing: SeparatorSpacingSize.Large }));
-            container.addTextDisplayComponents(new TextDisplayBuilder({ content: queueStr }));
+            container.addTextDisplayComponents(new TextDisplayBuilder({ content: queueData.queueString }));
             container.addSeparatorComponents(new SeparatorBuilder({ spacing: SeparatorSpacingSize.Large }));
-            container.addTextDisplayComponents(new TextDisplayBuilder({ content: `Page ${desiredPage}/${Math.ceil(player.queue.tracks.length / 10)} | Queue Length: ${msToTime(queueLength)}` }));
+            container.addTextDisplayComponents(new TextDisplayBuilder({ content: `Page ${desiredPage}/${Math.ceil(player.queue.tracks.length / 10)} | Queue Length: ${msToTime(queueData.queueLength)}` }));
             container.setAccentColor([10, 10, 10]);
 
-            await command.data.reply({
+            const previousPage = new ButtonBuilder()
+                .setLabel("Previous Page")
+                .setStyle(ButtonStyle.Primary)
+                .setCustomId("previous")
+                .setEmoji("⬅️");
+            if (desiredPage !== 0) previousPage.setDisabled(true);
+
+            const nextPage = new ButtonBuilder()
+                .setLabel("Next Page")
+                .setStyle(ButtonStyle.Primary)
+                .setCustomId("next")
+                .setEmoji("➡️");
+            if (desiredPage > Math.ceil(player.queue.tracks.length / 10)) nextPage.setDisabled(true);
+
+            const shuffleButton = new ButtonBuilder()
+                .setLabel("Shuffle Queue")
+                .setStyle(ButtonStyle.Secondary)
+                .setCustomId("shuffle")
+                .setEmoji("🔀");
+
+            container.addActionRowComponents(row => row.addComponents(previousPage, nextPage, shuffleButton));
+
+            const res = await command.data.reply({
                 flags: MessageFlags.IsComponentsV2,
                 components: [container],
+            });
+
+            const collector = res.createMessageComponentCollector({ time: 120_000 });
+
+            let shuffledCount = 0;
+
+            collector.on("collect", async (i) => {
+
+                if (!player) {
+                    previousPage.setDisabled(true);
+                    nextPage.setDisabled(true);
+                    shuffleButton.setDisabled(true);
+                    await i.update({ components: [container] });
+                    return;
+                }
+
+                if ((i.member as GuildMember).voice.channelId !== player.voiceChannelId) {
+                    await i.update({ components: [container] });
+                    return;
+                }
+
+                if (i.customId === "previous") curPage--;
+                if (i.customId === "next") curPage++;
+
+                if (i.customId === "shuffle") {
+                    await player.queue.shuffle();
+                    shuffledCount++;
+                    shuffleButton.setLabel(shuffledCount === 1 ? "Shuffled!" : `Shuffled again! (${shuffledCount}x)`);
+                    shuffleButton.setStyle(ButtonStyle.Success);
+                }
+
+                const newQueueData = generateQueueData(player, curPage);
+
+                container = new ContainerBuilder();
+
+                container.addTextDisplayComponents(new TextDisplayBuilder({ content: "## Queue:" }));
+                container.addSeparatorComponents(new SeparatorBuilder({ spacing: SeparatorSpacingSize.Large }));
+                container.addTextDisplayComponents(new TextDisplayBuilder({ content: newQueueData.queueString }));
+                container.addSeparatorComponents(new SeparatorBuilder({ spacing: SeparatorSpacingSize.Large }));
+                container.addTextDisplayComponents(new TextDisplayBuilder({ content: `Page ${curPage}/${Math.ceil(player.queue.tracks.length / 10)} | Queue Length: ${msToTime(newQueueData.queueLength)}` }));
+                container.setAccentColor([10, 10, 10]);
+
+                previousPage.setDisabled(curPage === 1);
+                nextPage.setDisabled((curPage === Math.ceil(player.queue.tracks.length / 10)));
+
+                container.addActionRowComponents(row => row.addComponents(previousPage, nextPage, shuffleButton));
+                await i.update({ components: [container] });
             });
         },
     } as ChatCommandOptions,
